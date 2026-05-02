fb1785dea27d47d40d5ce922342cc2ddfec1a5f6
YaswanthVanama <vanamayaswanth1@gmail.com>
2026-05-02 13:18:55 +0530

    Windows App configuration

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   assets: ['./src/assets/fonts/'],
+
+  dependencies: {
+    'react-native-document-picker': {
+      platforms: {
+        windows: null,
+      },
+    },
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        windows: null,
+      },
+    },
+    'react-native-screens': {
+      platforms: {
+        windows: null,
+      },
+    },
+  },
 };
